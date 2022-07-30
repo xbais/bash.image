0692d95044a6a62ad862538821a00e434641731e
Aakash Singh Bais <56718090+aakashsinghbais@users.noreply.github.com>
2022-07-30 17:16:52 +0530

    Created Python v-environment.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="Specify the input image to open.")
+args = parser.parse_args()
+print("The URL of the input image is = ", args.input)
